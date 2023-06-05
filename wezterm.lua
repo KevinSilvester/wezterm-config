0e4c0b075e9d3a20f78b6642a6a063ef0ac791c4
Kevin Silvester <kevinsilvester05@gmail.com>
2023-06-05 21:21:15 +0100

    feat: update file name

--- wezterm.lua
+++ wezterm.lua
@@ -6,7 +6,7 @@ require('events.tab-title').setup()
 return Config:init()
    :append(require('config.appearance'))
    :append(require('config.bindings'))
-   :append(require('config.domain'))
+   :append(require('config.domains'))
    :append(require('config.fonts'))
    :append(require('config.general'))
    :append(require('config.launch')).options

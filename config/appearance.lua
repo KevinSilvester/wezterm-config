8432cde89803229e921dd96161e7bd68c41f98dc
Kevin Silvester <kevinsilvester05@gmail.com>
2023-06-05 16:25:28 +0100

    fix: fix typo

--- config/appearance.lua
+++ config/appearance.lua
@@ -30,7 +30,7 @@ return {
    -- tab bar
    enable_tab_bar = true,
    hide_tab_bar_if_only_one_tab = false,
-   ues_fancy_tab_bar = false,
+   use_fancy_tab_bar = false,
    tab_max_width = 25,
    show_tab_index_in_tab_bar = false,
    switch_to_last_active_tab_when_closing_tab = true,

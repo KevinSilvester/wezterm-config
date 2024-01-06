b9c53d4115d5d7bc154e3308d597448d9ca83031
Kevin Silvester <kevinsilvester05@gmail.com>
2024-01-06 19:44:51 +0000

    revert font name

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-local font = 'JetBrainsMonoNL NF'
+local font = 'JetBrainsMono Nerd Font'
 local font_size = platform().is_mac and 12 or 9
 
 return {

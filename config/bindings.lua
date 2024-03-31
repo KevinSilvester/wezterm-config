a51a97682c1c6e2ea9dc6ad8f263f51e01ed2797
Kevin Silvester <kevinsilvester05@gmail.com>
2024-03-31 02:18:06 +0100

    fix: super keybinding for linux not set

--- config/bindings.lua
+++ config/bindings.lua
@@ -5,7 +5,7 @@ local act = wezterm.action
 
 local mod = {}
 
-if platform.is_mac then
+if platform.is_mac or platform.is_linux then
    mod.SUPER = 'SUPER'
    mod.SUPER_REV = 'SUPER|CTRL'
 elseif platform.is_win then

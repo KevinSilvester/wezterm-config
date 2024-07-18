6a59e51ef30629b25fd3fb6705001cb21f03b530
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-07-17 21:38:22 -0400

    changed font to MEsloLGS

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,12 +1,12 @@
 local wezterm = require('wezterm')
-local platform = require('utils.platform')
+-- local platform = require('utils.platform')
 
-local font = 'JetBrainsMono Nerd Font'
-local font_size = platform().is_mac and 12 or 9
+local font = 'MesloLGS Nerd Font'
+-- local font_size = platform().is_mac and 12 or 9
 
 return {
    font = wezterm.font(font),
-   font_size = font_size,
+   font_size = 13,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'

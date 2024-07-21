5f68894ac57536d43e8fe0eb1505324b62d07f4c
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-07-21 11:27:57 -0400

    New Branch:Macbook" For mac specific settings until I figure out lua condition based on OS

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 -- local platform = require('utils.platform')
 
-local font = 'MesloLGS Nerd Font'
+local font = 'MesloLGS NF'
 -- local font_size = platform().is_mac and 12 or 9
 
 return {

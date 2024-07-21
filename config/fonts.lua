d7c4153c48995a211a370f0044c46358dc7f01d3
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-07-21 11:46:42 -0400

    attempted fix of platform differentiation in fonts.lua

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,12 +1,19 @@
 local wezterm = require('wezterm')
--- local platform = require('utils.platform')
-
-local font = 'MesloLGS NF'
--- local font_size = platform().is_mac and 12 or 9
+local platform = require('utils.platform')
+local font
+local font_size
+local current_platform = platform()
+if current_platform.is_mac then
+   font = 'MesloLGS NF'
+   font_size = 13
+elseif current_platform.is_linux then
+   font = 'MesloLGS Nerd Font'
+   font_size = 15
+end
 
 return {
    font = wezterm.font(font),
-   font_size = 13,
+   font_size = font_size,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'

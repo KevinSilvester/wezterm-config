05e51b40f7e2e1ded53c69bea9e422a8cee204d5
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-07-21 21:56:36 -0400

    removed wsl config from domain

--- config/domains.lua
+++ config/domains.lua
@@ -4,15 +4,4 @@ return {
 
    -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
    unix_domains = {},
-
-   -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
-   wsl_domains = {
-      {
-         name = 'WSL:Ubuntu',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'fish', '-l' },
-      },
-   },
 }

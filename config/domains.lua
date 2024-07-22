b6b89fb471058f20d81aafcaeb7cc234028794ed
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-07-21 21:58:40 -0400

    confirmed that ssh is prepopulated agtain when his domains.lua is delted so removing it

--- config/domains.lua
+++
@@ -1,7 +0,0 @@
-return {
-   -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-   ssh_domains = {},
-
-   -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
-   unix_domains = {},
-}
--- wezterm.lua
+++ wezterm.lua
@@ -7,10 +7,12 @@ require('events.left-status').setup()
 require('events.tab-title').setup()
 require('events.new-tab-button').setup()
 
-return Config:init()
+return Config
+   :init()
    :append(require('config.appearance'))
    :append(require('config.bindings'))
-   :append(require('config.domains'))
    :append(require('config.fonts'))
    :append(require('config.general'))
-   :append(require('config.launch')).options
+   :append(require('config.launch'))
+   --   :append(require('config.domains'))
+   .options

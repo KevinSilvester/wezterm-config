7fe4650409a08a2eac290551de1a178aac052b3c
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-09-19 23:40:03 -0400

    readded domains.lua, commented out for now

---
+++ config/domains.lua
@@ -0,0 +1,18 @@
+--return {
+-- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
+--   ssh_domains = {},
+
+-- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
+--   unix_domains = {},
+
+-- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
+---   wsl_domains = {
+--      {
+--         name = 'WSL:Ubuntu',
+--         distribution = 'Ubuntu',
+--         username = 'wylde',
+--         default_cwd = '/home/wylde',
+--         default_prog = { 'fish', '-l' },
+--      },
+--   },
+--}

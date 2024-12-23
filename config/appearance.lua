8690835f654948f75023e1a6b5febfd7208de0da
Kevin Silvester <kevinsilvester05@gmail.com>
2024-12-23 21:10:52 +0000

    feat(config/appearance): increase underline thickness

--- config/appearance.lua
+++ config/appearance.lua
@@ -9,6 +9,7 @@ return {
    webgpu_preferred_adapter = gpu_adapters:pick_best(),
    -- webgpu_preferred_adapter = gpu_adapters:pick_manual('Dx12', 'IntegratedGpu'),
    -- webgpu_preferred_adapter = gpu_adapters:pick_manual('Gl', 'Other'),
+   underline_thickness = '1.5pt',
 
    -- cursor
    animation_fps = 120,

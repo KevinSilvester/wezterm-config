18803a73280bbf011e3f52e1357947c963765f11
Kevin Silvester <kevinsilvester05@gmail.com>
2024-12-23 21:10:13 +0000

    feat(config/domains): add wsl to ssh domains

--- config/domains.lua
+++ config/domains.lua
@@ -1,6 +1,16 @@
 return {
    -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-   ssh_domains = {},
+   -- ssh_domains = {},
+   ssh_domains = {
+      -- yazi's image on Windows will only work if launched in WSL
+      {
+         name = 'wsl.ssh',
+         remote_address = 'localhost',
+         multiplexing = 'None',
+         default_prog = { 'fish', '-l' },
+         assume_shell = 'Posix'
+      }
+   },
 
    -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
    unix_domains = {},

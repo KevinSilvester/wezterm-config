d357ab5499ca036f2bff8b2c1e1bd2aba016636d
Kevin Silvester <kevinsilvester05@gmail.com>
2026-04-14 01:58:53 +0100

    chore(events): remove redundant check default_action check

--- events/new-tab-button.lua
+++ events/new-tab-button.lua
@@ -98,7 +98,7 @@ M.setup = function()
          window:perform_action(default_action, pane)
       end
 
-      if default_action and button == 'Right' then
+      if button == 'Right' then
          window:perform_action(
             act.InputSelector({
                title = 'InputSelector: Launch Menu',
@@ -108,19 +108,16 @@ M.setup = function()
                action = wezterm.action_callback(function(_window, _pane, id, label)
                   if not id and not label then
                      return
-                  else
-                     wezterm.log_info('you selected ', id, label)
-                     wezterm.log_info(choices_data[tonumber(id)])
-                     window:perform_action(
-                        act.SpawnCommandInNewTab(choices_data[tonumber(id)]),
-                        pane
-                     )
                   end
+                  wezterm.log_info('you selected ', id, label)
+                  wezterm.log_info(choices_data[tonumber(id)])
+                  window:perform_action(act.SpawnCommandInNewTab(choices_data[tonumber(id)]), pane)
                end),
             }),
             pane
          )
       end
+
       return false
    end)
 end
